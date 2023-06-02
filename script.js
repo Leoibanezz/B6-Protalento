58c767989a2e3f1eb1c0d876a5823de208f27d69
Leoibanezz <leonardobuitrago620@gmail.com>
2023-06-02 18:20:00 -0500

    Estructuras_control

---
+++ script.js
@@ -0,0 +1,43 @@
+//*Texto en consola*//
+
+console.log("Hola mundo");
+
+//*Texto en Pagina*//
+
+document.write("Hola mundo x2");
+
+//*Texto en alertas*//
+
+//alert("Hola mundo x3");
+
+//string-palabra o mas palabras
+
+//Vamos a ocupar let o const
+//let cuando queramos actualizar el valor en algun otro momento
+//const cuando queramos mantener la estructura base de mi variable intacta
+
+
+let mensaje = "Hola como estan js.";
+
+const PI = 3.1416 
+
+// esto no se puede porque mi variable es una constante PI = 3.14
+mensaje = null;
+mensaje =undefined;
+
+document.write(mensaje);
+
+console.log(mensaje);
+
+
+let radio =4;
+
+console.log (2 * PI * (radio * radio));
+
+const perrito ={
+
+    nombre :"lola",
+    edad : 23,
+
+
+};;
