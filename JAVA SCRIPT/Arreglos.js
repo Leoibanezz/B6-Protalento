9fabc29523211f778b6277b92810bcd77c081ab9
Leoibanezz <leonardobuitrago620@gmail.com>
2023-06-07 01:06:44 -0500

    Arreglos js..

--- JAVA SCRIPT/Arreglos.js
+++ JAVA SCRIPT/Arreglos.js
@@ -1,10 +1,42 @@
-let numeros = [];
+//Ejercicio #1
 
-// Generar 10 números aleatorios y guardarlos en el array
+/*let numeros = [];
+
 for (let i = 0; i < 10; i++) {
-  let numeroAleatorio = Math.floor(Math.random() * 100); // Genera un número aleatorio entre 0 y 99
-  numeros.push(numeroAleatorio); // Agrega el número aleatorio al array
+  let numeroAleatorio = Math.floor(Math.random() * 20); 
+
+  numeros.push(numeroAleatorio);
 }
 
-// Mostrar el contenido del array en la consola
-console.log(numeros);
+console.log(numeros);*/
+
+//Ejercicio #2 El usuario deberá ingresar un string con varias palabras separadas por coma en un popup y se deben convertir en un array, (el usuario ingresa: '1,2,3,4,5' y se convierte en [1,2,3,4,5]). Mostrar en consola dicho resultado.
+
+
+/*let palabras = prompt ("Pon tus numeros separados por comas");
+
+let palabras2 = palabras.split(",");{
+
+
+} console.log (palabras2)*/
+
+// Ejercicio #3 
+
+// Definir el array
+/*let numeros = [10, 40, 30, 20, 15, 5];
+
+// Ordenar el arreglo de menor a mayor
+numeros.sort(function(b, a) {
+  return b - a;
+});
+
+// Mostrar el arreglo ordenado de menor a mayor
+console.log("Arreglo ordenado de menor a mayor: " + numeros);
+
+// Mostrar el número menor
+console.log("El número menor es: " + numeros[0]);
+
+// Mostrar el número mayor
+console.log("El número mayor es: " + numeros[numeros.length - 1]);*/
+
+
