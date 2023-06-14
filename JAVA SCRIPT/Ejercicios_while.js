36fcb3b85ff18de6f93152ae5d61fb77b0a149c7
Leoibanezz <leonardobuitrago620@gmail.com>
2023-06-13 23:50:46 -0500

    Ejercicios while

--- JAVA SCRIPT/Ejercicios_while.js
+++ JAVA SCRIPT/Ejercicios_while.js
@@ -1,4 +1,4 @@
-let numero = parseInt(prompt("Introduce un número: "));
+/*let numero = parseInt(prompt("Introduce un número: "));
 let result = 1;
 
 console.log("Los múltiplos de 5 hasta el número", numero, "son:");
@@ -7,4 +7,15 @@ while (result <= numero) {
     console.log(result);
   }
   result++;
-}
+}*/
+
+let numero = parseInt(prompt (`Introduce un numero `));
+
+let result = 1;
+
+console.log("los multiplos de 5 hasta el numero",numero , "son:");
+while (result <= numero) {
+    if (result % 5 === 0 ) {
+console.log(result);} result++; }
+
+
