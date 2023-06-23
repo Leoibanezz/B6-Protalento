a9c5cf942de2d2b2554e8039fb090c75cab4855b
Leoibanezz <leonardobuitrago620@gmail.com>
2023-06-22 22:25:29 -0500

    realizacion de ejercicios

--- JAVA SCRIPT/Ejercicios_while.js
+++ JAVA SCRIPT/Ejercicios_while.js
@@ -1,21 +1,38 @@
-/*let numero = parseInt(prompt("Introduce un número: "));
-let result = 1;
+// Ejercicio #1 Crea un programa que pregunte al usuario un número. Mostrar los números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario.
 
-console.log("Los múltiplos de 5 hasta el número", numero, "son:");
-while (result <= numero) {
-  if (result % 5 === 0) {
-    console.log(result);
-  }
-  result++;
-}*/
-
-let numero = parseInt(prompt (`Introduce un numero `));
+/*let numero = parseInt(prompt (`Introduce un numero `));
 
 let result = 1;
 
 console.log("los multiplos de 5 hasta el numero",numero , "son:");
 while (result <= numero) {
     if (result % 5 === 0 ) {
-console.log(result);} result++; }
+console.log(result);} result++; }*/
 
 
+// Ejercicio #2  Crea un programa que solicite al usuario 2 números entre 1 y 50. Posteriormente mostrar en consola los números del 1 hasta el 50, pero añadir el mensaje “¡Lotería!” solo al mostrar los números indicados por el usuario.
+
+/*numero1 = parseInt(prompt(`Introduce un primer numero entre 1 y 50`));
+numero2 = parseInt(prompt(`Introduce un segundo numero entre 1 y 50`));
+
+console.log("Los números de la lotería son:");
+
+let numero = 1;
+
+while (numero <= 50) {
+   
+  if (numero === numero1 || numero === numero2) {
+    console.log(numero + `loteria`);}
+     else {console.log(numero);}
+
+  numero ++;
+  
+}*/
+
+//Ejercicio #3 Crea un programa que solicite al usuario números, si lo que este introduce es un número guardarlo en un arreglo. Para terminar el capturar el usuario debe ingresar el número 0. Finalmente mostrar la lista de números capturados en pantalla o en la consola.
+
+
+for (let i = 1; i <=2; i++){
+  console.log(`iteracion ${i}`);
+
+} console.log(`fin del ciclo `)
